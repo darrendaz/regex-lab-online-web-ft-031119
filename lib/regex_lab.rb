f2265772abfcefe4d0155159ea17a9950c5475d7
Darren L <darren.lou@gmail.com>
2019-03-13 01:08:35 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,25 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  word.match(/\b[aeiouAEIOU]\w+/){|m|
+    return true
+  }
+  word.match(/\b[^aeiouAEIOU]\w+/){|m|
+    return false
+  }
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+\ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  return text.match(/^[A-Z].+[,.!?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/) ? true : false
 end
